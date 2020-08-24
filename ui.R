3ae6bb83ded6428731b4ec9a48bdeb2a083789c7
Pingping <mpp1992@foxmail.com>
2020-08-24 13:04:15 -0400

    add documentation

--- ui.R
+++ ui.R
@@ -7,6 +7,14 @@ shinyUI(fluidPage(
 
     # Application title
         titlePanel("Safe Distance"),
+        # Application document
+        mainPanel(h3("Instruction"),
+                  h4("The cars dataset was used to make the scatter plot, as shown below.
+                     Linear regression (red line) was used to fit the data to predict the safe stopping
+                     distance at different speeds. "),
+                  h4("Move the slider and set the speed. The safe distance predicted from the linear regression
+                  model will be shown using a green line. The safe distance from real data is shown using
+                  the blue line. If the input data is not included in the cars dataset, the blue line will not be shown")),
 
     # Sidebar with a slider input for number of bins
     sidebarLayout(
@@ -23,4 +31,5 @@ shinyUI(fluidPage(
             plotOutput("distPlot")
         )
     )
+    
 ))
